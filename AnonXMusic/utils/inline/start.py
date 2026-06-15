fba663c5d3c283839ecc1e915b03303e37da0864
superboygisan <superboygisan@gmail.com>
2026-06-16 00:47:25 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -1,39 +1,80 @@
-from pyrogram.types import InlineKeyboardButton
-
-import config
 from AnonXMusic import app
+import config
 
 
 def start_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-    ]
+
+    buttons = {
+        "inline_keyboard": [
+            [
+                {
+                    "text": f"🌛 {_['S_B_1']}",
+                    "url": f"https://t.me/{app.username}?startgroup=true",
+                    "style": "success"
+                },
+                {
+                    "text": f"🌛 {_['S_B_2']}",
+                    "url": config.SUPPORT_CHAT,
+                    "style": "primary"
+                },
+            ],
+        ]
+    }
+
     return buttons
 
 
 def private_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_5"], url=config.SUPPORT_CHANNEL),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [InlineKeyboardButton(text="• LALA LALA  •", url=f"CURRENCY")],
-    
-    ]
-    
+
+    buttons = {
+        "inline_keyboard": [
+
+            [
+                {
+                    "text": f"🌛 {_['S_B_3']}",
+                    "url": f"https://t.me/{app.username}?startgroup=true",
+                    "style": "success"
+                }
+            ],
+
+            [
+                {
+                    "text": f"🌛 {_['S_B_4']}",
+                    "callback_data": "settings_back_helper",
+                    "style": "primary"
+                }
+            ],
+
+            [
+                {
+                    "text": f"🌛 {_['S_B_6']}",
+                    "user_id": config.OWNER_ID,
+                    "style": "primary"
+                },
+                {
+                    "text": f"🌛 {_['S_B_5']}",
+                    "url": config.SUPPORT_CHANNEL,
+                    "style": "primary"
+                },
+            ],
+
+            [
+                {
+                    "text": f"🌛 {_['S_B_2']}",
+                    "url": config.SUPPORT_CHAT,
+                    "style": "primary"
+                },
+            ],
+
+            [
+                {
+                    "text": "🌛 • LALA LALA •",
+                    "url": "CURRENCY",
+                    "style": "danger"
+                }
+            ],
+
+        ]
+    }
+
     return buttons
