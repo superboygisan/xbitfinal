dace4a57ee22b08774dbae6630c9c4f0373dc085
superboygisan <superboygisan@gmail.com>
2026-06-16 05:13:57 +0530

    Update __init__.py

--- AnonXMusic/utils/inline/__init__.py
+++ AnonXMusic/utils/inline/__init__.py
@@ -5,3 +5,23 @@ from .queue import *
 from .settings import *
 from .speed import *
 from .start import *
+
+# Explicitly import all functions from play.py to avoid ImportError
+from .play import (
+    track_markup,
+    stream_markup,
+    stream_markup_timer,
+    playlist_markup,
+    livestream_markup,
+    slider_markup,
+)
+
+__all__ = [
+    "track_markup",
+    "stream_markup",
+    "stream_markup_timer",
+    "playlist_markup",
+    "livestream_markup",
+    "slider_markup",
+    # other imports if needed
+]
