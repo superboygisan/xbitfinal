3a2aab2d69db46649f1ace09793265b0190645a9
superboygisan <superboygisan@gmail.com>
2026-06-16 13:49:16 +0530

    Update __init__.py

--- AnonXMusic/utils/inline/__init__.py
+++ AnonXMusic/utils/inline/__init__.py
@@ -1,28 +1,28 @@
 from .extras import *
 from .help import *
-from .play import *
+# play.py se direct functions nikalne ke bajay hum sirf 'Inline' class ko import karenge
+from .play import Inline
 from .queue import *
 from .settings import *
 from .speed import *
 from .start import *
 
-# Explicitly import all functions from play.py to avoid ImportError
-from .play.inline import (
-track_markup,
-stream_markup,
-stream_markup_timer,
-playlist_markup,
-livestream_markup,
-slider_markup,)
+# Yahan humne class ko mention karke uska object bana diya
+buttons = Inline()
 
+# Ab us class ke andar ke saare functions ko explicitly mention kar dete hain
+track_markup = buttons.track_markup
+stream_markup = buttons.stream_markup
+stream_markup_timer = buttons.stream_markup_timer
+playlist_markup = buttons.playlist_markup
+livestream_markup = buttons.livestream_markup
+slider_markup = buttons.slider_markup
 
 _all_ = [
-"track_markup",
-"stream_markup",
-"stream_markup_timer",
-"playlist_markup",
-"livestream_markup",
-"slider_markup",
+    "track_markup",
+    "stream_markup",
+    "stream_markup_timer",
+    "playlist_markup",
+    "livestream_markup",
+    "slider_markup",
 ]
-
-buttons = Inline()
